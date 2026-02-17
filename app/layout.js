a7a5a881eeef89e6f6cfb01dad153393ef18d23a
Lakshay229 <lakshaysingla840@gmail.com>
2026-02-18 00:53:49 +0530

    chore: update dependencies and add new packages for improved functionality
    - Added @clerk/nextjs, class-variance-authority, clsx, convex, lucide-react, and tailwind-merge to dependencies.
    - Updated react and react-dom to version 18.
    - Added shadcn and tw-animate-css to devDependencies.
    - Updated eslint-config-next to version 16.1.6.

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Outfit } from "next/font/google";
+import Provider from "./provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const outfit = Outfit({subsets: ["latin"]});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,9 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={outfit.className}
       >
-        {children}
+        <Provider>
+          {children}
+        </Provider>
       </body>
     </html>
   );
